3141d6d1c0b20734e9d19580711c7588b822523b
devopsadmin <farmaan78678@gmail.com>
2024-12-14 20:23:32 +0530

    new terraform directory under repo

---
+++ prod/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install curl wget net-tools zip -y
+#sudo hostnamectl set-hostname ansible-node-1
+sudo adduser --disabled-password --gecos "" devops && echo "devops:admin" | sudo chpasswd && echo "devops ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/devops
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config.d/*.conf
+sudo systemctl daemon-reload
+sudo systemctl restart ssh
+#sudo apt install nginx -y
+#sudo systemctl enable nginx
